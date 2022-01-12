bb1cbee35e718b1750a5cfa4ad01cba1f813636c
Geoffrey Villard <agence.idneed@gmail.com>
2022-01-12 15:34:04 +0100

    add dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGRUqj42db9BeNPPOxKihY73pT93d0r9k",
+  authDomain: "trello-project-bac7d.firebaseapp.com",
+  projectId: "trello-project-bac7d",
+  storageBucket: "trello-project-bac7d.appspot.com",
+  messagingSenderId: "446819331212",
+  appId: "1:446819331212:web:6348faa3ccdfce9b769fa9"
+};
+
+const app = initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
