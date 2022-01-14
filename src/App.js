4c1dacc839e8d1330c414e484015e5612421ba20
damlaSub <subasi.paris@gmail.com>
2022-01-14 12:04:35 +0100

    change to package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCGRUqj42db9BeNPPOxKihY73pT93d0r9k",
-  authDomain: "trello-project-bac7d.firebaseapp.com",
-  projectId: "trello-project-bac7d",
-  storageBucket: "trello-project-bac7d.appspot.com",
-  messagingSenderId: "446819331212",
-  appId: "1:446819331212:web:6348faa3ccdfce9b769fa9"
-};
 
-const app = initializeApp(firebaseConfig);
+ const firebaseConfig = {
+   apiKey: "AIzaSyCGRUqj42db9BeNPPOxKihY73pT93d0r9k",
+   authDomain: "trello-project-bac7d.firebaseapp.com",
+   projectId: "trello-project-bac7d",
+   storageBucket: "trello-project-bac7d.appspot.com",
+   messagingSenderId: "446819331212",
+   appId: "1:446819331212:web:6348faa3ccdfce9b769fa9"
+ };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//  const App = initializeApp(firebaseConfig);
+
+ function App() {
+    return ();
+ } 
 
 export default App;
